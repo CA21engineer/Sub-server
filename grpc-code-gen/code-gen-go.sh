f3d3eeb6fde9b2d1fff51a3dec81dce2e82a6527
Suzuki Takeo <takeo@stko.info>
2020-06-07 20:28:44 +0900

    code gen用スクリプト作成

--- grpc-code-gen/code-gen-go.sh
+++ grpc-code-gen/code-gen-go.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-docker build `dirname $0` -t protoc-gen-grpc-web:latest
+NAME_IMAGE='protoc-gen-grpc-web:latest'
 
-mkdir -p $2
-for file in `\find $1 -name '*.proto'`; do
-    docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:/${PWD}" -w="/${PWD}" protoc-gen-grpc-web \
-    protoc \
-    -I$1 \
-    -I/usr/local/include/google \
-    --plugin=protoc-gen-grpc=/usr/local/bin/grpc_csharp_plugin \
-    --csharp_out=$2 \
-    --grpc_out=$2 \
-    $file
-done
+function build () {
+  mkdir -p $2
+  for file in `\find $1 -name '*.proto'`; do
+      docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:/${PWD}" -w="/${PWD}" protoc-gen-grpc-web \
+      protoc \
+      -I$1 \
+      -I/usr/local/include/google \
+      --go_out=plugins=grpc:$2 \
+      $file
+  done
+}
+
+
+if [ "$(docker image ls -q ${NAME_IMAGE})" ]; then
+  echo "Image ${NAME_IMAGE} already exist."
+else
+  docker build `dirname $0` -t protoc-gen-grpc-web:latest
+fi
+
+build $1 $2
