4cf485b6c8947cb4d3d23ad5cd39a1081c50bdc4
takehanKosuke <kkoussuke530@icloud.com>
2020-06-09 21:51:28 +0900

    lintter調整

--- apiServer/models/icon.go
+++ apiServer/models/icon.go
@@ -1,20 +1,19 @@
 package models
 
 type Icon struct {
-	IconId string `gorm:`
-	IconUri string
+	IconId     string
+	IconUri    string
 	IsOriginal bool
 }
 
 type Icons []*Icon
-
 
 func NewIcon(id, uri string, is_original bool) *Icon {
 	return &Icon{id, uri, is_original}
 }
 
 // All 全てのiconを取得
-func (i *Icon) All()([]*Icon, error){
+func (i *Icon) All() ([]*Icon, error) {
 	var icons Icons
 	if err := DB.Find(&icons).Error; err != nil {
 		return nil, err
--- apiServer/models/subscription.go
+++ apiServer/models/subscription.go
@@ -2,20 +2,20 @@ package models
 
 const (
 	NOT_FOUND = iota + 1
-  NOT_CATEGORIZED
-  MUSIC
-  MOVIE
-  MATCHING
-  STORAGE
+	NOT_CATEGORIZED
+	MUSIC
+	MOVIE
+	MATCHING
+	STORAGE
 )
 
 type Subscription struct {
 	SubscriptionId string
-	IconId string
-	ServiceName string
-	ServiceType int32
-	Price int32
-	Cycle int32
-	IsOriginal bool
-	FreeTrial int32
+	IconId         string
+	ServiceName    string
+	ServiceType    int32
+	Price          int32
+	Cycle          int32
+	IsOriginal     bool
+	FreeTrial      int32
 }
