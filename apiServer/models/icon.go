411f5eb9e6ba1c88527fe61e2ce041cb44589ed9
Suzuki Takeo <takeo@stko.info>
2020-06-11 04:25:13 +0900

    sql問題の解決案

--- apiServer/models/icon.go
+++ apiServer/models/icon.go
@@ -1,15 +1,19 @@
 package models
 
+import "time"
+
 // Icon struct
 type Icon struct {
 	IconID     string
 	IconURI    string
 	IsOriginal bool
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 }
 
 // NewIcon New Icon struct
 func NewIcon(id, uri string, isOriginal bool) *Icon {
-	return &Icon{id, uri, isOriginal}
+	return &Icon{id, uri, isOriginal, time.Now(), time.Now()}
 }
 
 // All 全てのiconを取得
--- apiServer/models/subscription.go
+++ apiServer/models/subscription.go
@@ -2,17 +2,24 @@ package models
 
 import (
 	subscription "github.com/CA21engineer/Subs-server/apiServer/pb"
+	"time"
 )
 
 // Subscription Subscription struct
 type Subscription struct {
 	SubscriptionID string
 	IconID         string
-	Icon           *Icon `gorm:"-"`
 	ServiceName    string
 	ServiceType    subscription.ServiceType
 	Price          int32
 	Cycle          int32
 	IsOriginal     bool
 	FreeTrial      int32
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type SubscriptionWithIcon struct {
+	Subscription
+	Icon
 }
