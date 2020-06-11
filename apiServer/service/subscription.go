424fe7418c1fc20c1b3a84eb275e81fbd8754b9b
takehanKosuke <kkoussuke530@icloud.com>
2020-06-11 11:33:57 +0900

    不要なリクエストインターフェースを削除

--- apiServer/service/subscription.go
+++ apiServer/service/subscription.go
@@ -21,12 +21,12 @@ func (SubscriptionServiceImpl) GetIconImageList(ctx context.Context, req *subscr
 }
 
 // GetSubscriptions サーバーに登録済みのサブスク一覧
-func (SubscriptionServiceImpl) GetSubscriptions(context.Context, *subscription.GetSubscriptionsRequest) (*subscription.GetSubscriptionsResponse, error) {
+func (SubscriptionServiceImpl) GetSubscriptions(ctx context.Context, req *subscription.Empty) (*subscription.GetSubscriptionsResponse, error) {
 	return &subscription.GetSubscriptionsResponse{}, nil
 }
 
 // GetPopularSubscriptions サーバーに登録済みのサブスク一覧を人気順で取得
-func (SubscriptionServiceImpl) GetPopularSubscriptions(context.Context, *subscription.GetPopularSubscriptionsRequest) (*subscription.GetPopularSubscriptionsResponse, error) {
+func (SubscriptionServiceImpl) GetPopularSubscriptions(ctx context.Context, req *subscription.Empty) (*subscription.GetPopularSubscriptionsResponse, error) {
 	return &subscription.GetPopularSubscriptionsResponse{}, nil
 }
 
