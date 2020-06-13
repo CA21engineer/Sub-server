6caa4e95b5410709114aaa2e61ec8431b61dfab3
nakao107107 <imakeanewstory@gmail.com>
2020-06-13 14:37:59 +0900

    [WIP]fix

--- apiServer/adopter/user_subscription.go
+++ apiServer/adopter/user_subscription.go
@@ -3,23 +3,16 @@ package adopter
 import (
 	"github.com/CA21engineer/Subs-server/apiServer/models"
 	subscription "github.com/CA21engineer/Subs-server/apiServer/pb"
-	"github.com/golang/protobuf/ptypes"
 )
 
 // ConvertGRPCUserSubscriptionResponse `*models.UserSubscription`を`*subscription.UserSubscription`に変換
 func ConvertGRPCUserSubscriptionResponse(s *models.UserSubscription) *subscription.UserSubscription {
-	startedAt, _ := ptypes.TimestampProto(s.StartedAt)
+	subscriptionWithIcon := models.SubscriptionWithIcon{
+		Subscription: s.Subscription,
+	}
 	return &subscription.UserSubscription{
 		UserSubscriptionId:s.UserSubscriptionID,
-		SubscriptionId: s.Subscription.SubscriptionID,
-		ServiceType:    s.Subscription.ServiceType,
-		IconUri:        s.Icon.IconURI,
-		ServiceName:    s.Subscription.ServiceName,
-		Price:          s.Price,
-		Cycle:          s.Cycle,
-		FreeTrial:      s.Subscription.FreeTrial,
-		IsOriginal:     s.Subscription.IsOriginal,
-		StartedAt:      startedAt,
+		Subscription: ConvertGRPCSubscriptionResponse(&subscriptionWithIcon),
 	}
 }
 
